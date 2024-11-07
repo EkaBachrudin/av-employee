db8f72c0b2b6ebcd392b04573356bb793471fffa
randinovranda <vvalentine825@gmail.com>
2024-11-07 13:49:23 +0700

    update package.json

--- src/app/components/layout-page/layout-page.component.ts
+++ src/app/components/layout-page/layout-page.component.ts
@@ -1,12 +1,22 @@
 import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-layout-page',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './layout-page.component.html',
   styleUrl: './layout-page.component.scss'
 })
 export class LayoutPageComponent {
+  constructor(private authService: AuthService){
+
+  }
+
+  logout() {
+    this.authService.logout();
+    window.location.reload();
+  }
   @Input() title!: string;
 }
